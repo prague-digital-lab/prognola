005794d8f5afc9a9365352fe389b42bc0a6f4e7c
Michael Dojčár <michaeldojcar@icloud.com>
2024-06-10 21:09:49 +0200

    update env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,22 @@
 export default defineNuxtConfig({
     devtools: {enabled: true},
     css: ['~/assets/scss/app.scss'],
+
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
     },
+
     runtimeConfig: {
         public: {
-            apiBase: 'http://valasskapevnost.cz.test/api' // NUXT_PUBLIC_API_BASE env
+            apiBase: 'http://valasskapevnost.cz.test/api', // NUXT_PUBLIC_API_BASE env
+            sanctum: {
+                baseUrl: 'http://valasskapevnost.cz.test/api', // NUXT_PUBLIC_SANCTUM_BASE_URL=
+            },
         }
     },
+
+    modules: ["nuxt-auth-sanctum"]
 })
