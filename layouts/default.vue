b10c8d6d919a49902ce3f134173c7f683b092ffa
Michael Dojčár <michaeldojcar@icloud.com>
2024-07-25 13:54:14 +0200

    ref VP-286 expense index

--- layouts/default.vue
+++ layouts/default.vue
@@ -144,7 +144,7 @@
                                  aria-hidden="true"/>
             <input id="search-field"
                    class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                   placeholder="Search..." type="search" name="search"/>
+                   placeholder="Vyhledávání" type="search" name="search"/>
           </form>
           <div class="flex items-center gap-x-4 lg:gap-x-6">
             <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
