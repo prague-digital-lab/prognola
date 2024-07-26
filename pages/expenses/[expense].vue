929503338f67845b6ae53c29097616f240c82c38
Michael Dojčár <michaeldojcar@icloud.com>
2024-07-26 13:08:50 +0200

    ref PRO-79 expense detail ui

--- pages/expenses/[expense].vue
+++ pages/expenses/[expense].vue
@@ -1,20 +1,45 @@
 <template>
   <div v-if="loaded">
     <div class="md:flex md:justify-between mb-4 divide-x divide-slate-100 h-auto">
-      <div class="">
+      <div class="w-full">
         <input type="text"
                class="p-0 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight mb-3 border-none focus:ring-0"
                placeholder="Název výdaje" v-model="input_description">
 
-        <p class="text-gray-500 text-sm">Výdaj</p>
+        <p class="text-gray-500 text-sm mb-5">Výdaj V-{{ expense.id }}</p>
+
+        <textarea v-model="input_internal_note" class="border-none focus:ring-0 p-0 text-sm text-slate-700 w-full"
+                  placeholder="Přidat popis..."></textarea>
       </div>
 
       <div class="w-[250px] ps-4">
 
-        <p class="text-sm py-1 rounded text-gray-800 hover:bg-gray-100" v-if="expense.organisation">
+        <p class="text-base py-1 rounded text-gray-800 hover:bg-gray-100 mb-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+               stroke="currentColor" class="size-5 inline-block me-2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"/>
+          </svg>
+
+          2 543 Kč
+        </p>
+
+        <p class="text-sm py-1 rounded text-gray-800 hover:bg-gray-100 mb-4">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+               class="size-5 inline-block text-orange-400 me-2">
+            <path fill-rule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.902 7.098a3.75 3.75 0 0 1 3.903-.884.75.75 0 1 0 .498-1.415A5.25 5.25 0 0 0 8.005 9.75H7.5a.75.75 0 0 0 0 1.5h.054a5.281 5.281 0 0 0 0 1.5H7.5a.75.75 0 0 0 0 1.5h.505a5.25 5.25 0 0 0 6.494 2.701.75.75 0 1 0-.498-1.415 3.75 3.75 0 0 1-4.252-1.286h3.001a.75.75 0 0 0 0-1.5H9.075a3.77 3.77 0 0 1 0-1.5h3.675a.75.75 0 0 0 0-1.5h-3c.105-.14.221-.274.348-.402Z"
+                  clip-rule="evenodd"/>
+          </svg>
+
+
+          Není zaplacené
+        </p>
+
+        <p class="text-sm py-1 rounded text-gray-800 hover:bg-gray-100 mb-4" v-if="expense.organisation">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                stroke="currentColor"
-               class="size-4 inline-block me-2">
+               class="size-5 inline-block me-2">
             <path stroke-linecap="round" stroke-linejoin="round"
                   d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"/>
           </svg>
@@ -22,16 +47,57 @@
           {{ expense.organisation.name }}
         </p>
 
-        <p class="text-sm py-1 rounded text-gray-600 hover:bg-gray-100" v-else>
+        <p class="text-sm py-1 rounded text-gray-600 hover:bg-gray-100 mb-7" v-else>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                stroke="currentColor"
-               class="size-4 inline-block me-2">
+               class="size-5 inline-block me-2">
             <path stroke-linecap="round" stroke-linejoin="round"
                   d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"/>
           </svg>
 
           zvolit organizaci
         </p>
+
+        <p class="text-xs font-medium text-gray-500 mb-2">Kategorie výdaje</p>
+
+        <p class="text-xs py-1 rounded text-gray-800 hover:bg-gray-100 mb-7">
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+               class="size-5 text-green-600 inline-block me-2">
+            <path fill-rule="evenodd"
+                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"
+                  clip-rule="evenodd"/>
+          </svg>
+
+
+          Marketing <span class="mx-1 text-slate-400">></span>
+          PPC reklama
+        </p>
+
+        <p class="text-xs font-medium text-gray-500 mb-2">Platby</p>
+
+        <p class="text-xs py-1 rounded text-gray-800 hover:bg-gray-100 mb-1">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+               class="size-5 text-black inline-block me-2">
+            <path fill-rule="evenodd"
+                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"
+                  clip-rule="evenodd"/>
+          </svg>
+
+          -2000 Kč
+        </p>
+
+        <p class="text-xs py-1 rounded text-gray-800 hover:bg-gray-100 mb-1">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+               class="size-5 text-black inline-block me-2">
+            <path fill-rule="evenodd"
+                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"
+                  clip-rule="evenodd"/>
+          </svg>
+
+         -543 Kč
+        </p>
+
       </div>
 
     </div>
@@ -46,7 +112,8 @@ export default {
       loaded: false,
 
       expense: null,
-      input_description: null,
+      input_description: '',
+      input_internal_note: '',
     }
   },
 
@@ -68,6 +135,7 @@ export default {
 
       this.expense = data.value
       this.input_description = data.value.description
+      this.input_internal_note = data.value.internal_note
 
       this.loaded = true
     },
