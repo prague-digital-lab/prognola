aae6150b9aa5f1b767fb062b02cfbee7d15be958
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-05 16:12:31 +0200

    expense ui

---
+++ components/expense/ExpenseOptions.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  props: ['expense'],
+
+  data: () => {
+    return {
+      expanded: false,
+    }
+  },
+
+  methods: {
+    expand() {
+      this.expanded = true
+    },
+
+    close() {
+      this.expanded = false
+    },
+
+    async deleteExpense() {
+      if (!confirm("Opravdu chcete odstranit tento výdaj?")) {
+        return;
+      }
+
+      const client = useSanctumClient();
+
+      const {data} = await useAsyncData('expense', () =>
+          client('/api/expenses/' + this.expense.id, {
+            method: 'DELETE'
+          })
+      )
+
+      await navigateTo('/expenses')
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="relative">
+    <p class="text-sm py-1 px-1 rounded text-gray-700 hover:bg-gray-100 mb-3" @click="expanded ? close() : expand()">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+           stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round"
+              d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/>
+      </svg>
+    </p>
+
+
+    <Transition>
+      <div v-if="expanded"
+           class="absolute left-[-150px] top-[-5px] w-[147px] bg-white border-slate-200 border shadow rounded px-1 py-1">
+
+        <p class="p-2 hover:bg-gray-100 text-red-600" @click="deleteExpense">Odstranit výdaj</p>
+      </div>
+    </Transition>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/expenses/[expense].vue
+++ pages/expenses/[expense].vue
@@ -2,9 +2,16 @@
   <div v-if="loaded">
     <div class="md:flex md:justify-between mb-4 divide-x divide-slate-100 h-auto">
       <div class="w-full me-5">
-        <input type="text"
-               class="p-0 w-full text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight mb-3 border-none focus:ring-0"
-               placeholder="Název výdaje" v-model="input_description" v-on:blur="updateDescription">
+        <div class="flex justify-between">
+          <input type="text"
+                 class="p-0 w-full text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight mb-3 border-none focus:ring-0"
+                 placeholder="Název výdaje" v-model="input_description" v-on:blur="updateDescription">
+
+          <div>
+            <expense-options :expense="expense"/>
+          </div>
+
+        </div>
 
         <p class="text-gray-500 text-sm mb-5">Výdaj V-{{ expense.id }}</p>
 
@@ -64,34 +71,37 @@
           Zvolit kategorii
         </p>
 
-<!--        <p class="text-xs px-1 font-medium text-gray-500 mb-2">Platby</p>-->
+        <!--        <p class="text-xs px-1 font-medium text-gray-500 mb-2">Platby</p>-->
 
-<!--        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-1">-->
-<!--          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"-->
-<!--               class="size-5 text-gray-600 inline-block me-2">-->
-<!--            <path fill-rule="evenodd"-->
-<!--                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"-->
-<!--                  clip-rule="evenodd"/>-->
-<!--          </svg>-->
+        <!--        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-1">-->
+        <!--          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"-->
+        <!--               class="size-5 text-gray-600 inline-block me-2">-->
+        <!--            <path fill-rule="evenodd"-->
+        <!--                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"-->
+        <!--                  clip-rule="evenodd"/>-->
+        <!--          </svg>-->
 
-<!--          -2000 Kč-->
-<!--        </p>-->
+        <!--          -2000 Kč-->
+        <!--        </p>-->
 
-<!--        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-4">-->
-<!--          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"-->
-<!--               class="size-5 text-gray-600 inline-block me-2">-->
-<!--            <path fill-rule="evenodd"-->
-<!--                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"-->
-<!--                  clip-rule="evenodd"/>-->
-<!--          </svg>-->
+        <!--        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-4">-->
+        <!--          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"-->
+        <!--               class="size-5 text-gray-600 inline-block me-2">-->
+        <!--            <path fill-rule="evenodd"-->
+        <!--                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"-->
+        <!--                  clip-rule="evenodd"/>-->
+        <!--          </svg>-->
 
-<!--          -543 Kč-->
-<!--        </p>-->
+        <!--          -543 Kč-->
+        <!--        </p>-->
 
         <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'paid'">Uhrazeno</p>
-        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'plan'">Plánovaná úhrada</p>
-        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'draft'">Plánovaná úhrada</p>
-        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'pending'">Plánovaná úhrada</p>
+        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'plan'">Plánovaná
+          úhrada</p>
+        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'draft'">Plánovaná
+          úhrada</p>
+        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'pending'">Plánovaná
+          úhrada</p>
 
         <expense-paid-at-input :expense="expense"/>
 
@@ -170,7 +180,7 @@ export default {
             }
           })
       )
-    }
+    },
   }
 }
 
