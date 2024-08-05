c2301f1cce7e172188c2eda43e925b6f71e82927
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-05 15:56:55 +0200

    update paid_at

--- pages/expenses/[expense].vue
+++ pages/expenses/[expense].vue
@@ -52,40 +52,48 @@
         <p class="text-xs px-1 py-1 rounded text-gray-800 hover:bg-gray-100 mb-7">
 
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-               class="size-5 text-green-600 inline-block me-2">
+               class="size-5 text-gray-600 inline-block me-2">
             <path fill-rule="evenodd"
                   d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"
                   clip-rule="evenodd"/>
           </svg>
 
 
-          Marketing <span class="mx-1 text-slate-400">></span>
-          PPC reklama
+          <!--          Marketing <span class="mx-1 text-slate-400">></span>-->
+          <!--          PPC reklama-->
+          Zvolit kategorii
         </p>
 
-        <p class="text-xs px-1 font-medium text-gray-500 mb-2">Platby</p>
+<!--        <p class="text-xs px-1 font-medium text-gray-500 mb-2">Platby</p>-->
 
-        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-1">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-               class="size-5 text-black inline-block me-2">
-            <path fill-rule="evenodd"
-                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"
-                  clip-rule="evenodd"/>
-          </svg>
+<!--        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-1">-->
+<!--          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"-->
+<!--               class="size-5 text-gray-600 inline-block me-2">-->
+<!--            <path fill-rule="evenodd"-->
+<!--                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"-->
+<!--                  clip-rule="evenodd"/>-->
+<!--          </svg>-->
 
-          -2000 Kč
-        </p>
+<!--          -2000 Kč-->
+<!--        </p>-->
 
-        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-1">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-               class="size-5 text-black inline-block me-2">
-            <path fill-rule="evenodd"
-                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"
-                  clip-rule="evenodd"/>
-          </svg>
+<!--        <p class="text-xs py-1 px-1 rounded text-gray-800 hover:bg-gray-100 mb-4">-->
+<!--          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"-->
+<!--               class="size-5 text-gray-600 inline-block me-2">-->
+<!--            <path fill-rule="evenodd"-->
+<!--                  d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"-->
+<!--                  clip-rule="evenodd"/>-->
+<!--          </svg>-->
 
-          -543 Kč
-        </p>
+<!--          -543 Kč-->
+<!--        </p>-->
+
+        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'paid'">Uhrazeno</p>
+        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'plan'">Plánovaná úhrada</p>
+        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'draft'">Plánovaná úhrada</p>
+        <p class="text-xs px-1 font-medium text-gray-500 mb-2" v-if="expense.payment_status === 'pending'">Plánovaná úhrada</p>
+
+        <expense-paid-at-input :expense="expense"/>
 
       </div>
 
@@ -95,9 +103,10 @@
 
 <script>
 import ExpenseStatusSelect from "~/components/expense/ExpenseStatusSelect.vue";
+import ExpensePaidAtInput from "~/components/expense/ExpensePaidAtInput.vue";
 
 export default {
-  components: {ExpenseStatusSelect},
+  components: {ExpensePaidAtInput, ExpenseStatusSelect},
   data() {
     return {
       route: null,
@@ -157,7 +166,9 @@ export default {
           client('/api/expenses/' + this.route.params.expense, {
             method: 'PATCH',
             body: {
-              internal_note: this.input_internal_note
+              internal_note: this.input_internal_note,
+              received_at: this.from,
+              paid_at: this.from
             }
           })
       )
