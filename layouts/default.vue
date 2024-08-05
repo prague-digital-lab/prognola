b4cf392c3b5088ac5a8c73641753e2015b4ac0ef
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-05 16:51:30 +0200

    ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -141,11 +141,11 @@
         <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
           <form class="relative flex flex-1" action="#" method="GET">
             <label for="search-field" class="sr-only">Search</label>
-            <MagnifyingGlassIcon class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-                                 aria-hidden="true"/>
-            <input id="search-field"
-                   class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                   placeholder="Vyhledávání" type="search" name="search"/>
+<!--            <MagnifyingGlassIcon class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"-->
+<!--                                 aria-hidden="true"/>-->
+<!--            <input id="search-field"-->
+<!--                   class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"-->
+<!--                   placeholder="Vyhledávání" type="search" name="search"/>-->
           </form>
           <div class="flex items-center gap-x-4 lg:gap-x-6">
             <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
