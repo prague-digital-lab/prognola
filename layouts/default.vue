01e2543778b24c133982c38af4b14f07013d61d2
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-05 16:44:29 +0200

    user profile photo

--- layouts/default.vue
+++ layouts/default.vue
@@ -161,7 +161,7 @@
               <MenuButton class="-m-1.5 flex items-center p-1.5">
                 <span class="sr-only">Open user menu</span>
                 <img class="h-8 w-8 rounded-full bg-gray-50"
-                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                     :src="user.profile_photo_url"
                      alt=""/>
                 <span class="hidden lg:flex lg:items-center">
                   <span class="ml-4 text-sm font-semibold leading-6 text-gray-900"
