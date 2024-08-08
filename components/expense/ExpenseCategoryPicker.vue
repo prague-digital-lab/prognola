ea93f8d24600ab801947ce9f75b310de0a20d193
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-08 11:46:08 +0200

    fix PRO-97 category picker - group categories by department

--- components/expense/ExpenseCategoryPicker.vue
+++ components/expense/ExpenseCategoryPicker.vue
@@ -61,10 +61,18 @@ export default {
 
   computed: {
     filtered_categories() {
-      return this.expense_categories.filter( category => {
+      return this.expense_categories.filter(category => {
         // console.log(category.name, this.category_name_filter)
         return !this.category_name_filter || category.name.toLowerCase().indexOf(this.category_name_filter.toLowerCase()) > -1
       })
+    },
+
+    filtered_categories_grouped_by_department() {
+      return this.filtered_categories.reduce(function (r, category) {
+        r[category.department_id] = r[category.department_id] || [];
+        r[category.department_id].push(category);
+        return r;
+      }, Object.create(null))
     }
   }
 }
@@ -97,9 +105,22 @@ export default {
         <input class="rounded w-full mb-2" type="text" placeholder="Název kategorie..." v-model="category_name_filter">
 
         <div class="max-h-[50vh] overflow-auto">
-        <p class="px-2 py-1 hover:bg-gray-100 text-gray-700 text-sm" v-for="category in filtered_categories"
-           @click="selectCategory(category)">
-          {{ category.name }}</p>
+          <div v-for="department in filtered_categories_grouped_by_department">
+            <p class="px-2 py-1 text-gray-500 text-xs">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 inline-block">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+              </svg>
+
+
+              {{department[0].department.name}}</p>
+
+          <p class="px-2 py-1 hover:bg-gray-100 text-gray-700 text-sm" v-for="category in department"
+             @click="selectCategory(category)">
+            {{ category.name }}</p>
+          </div>
+
+          <p class="px-2 py-1 text-gray-500 text-sm" v-if="filtered_categories.length === 0">
+            žádná kategorie</p>
         </div>
       </div>
     </Transition>
