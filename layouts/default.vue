e3626c45d2b9302d0a54e6791d89f76ae2e863e6
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-18 18:30:06 +0200

    update ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -85,7 +85,7 @@
       <div class="flex grow bg-slate-50 flex-col gap-y-5 overflow-y-auto border-r border-gray-200 px-6 pb-4">
         <div class="flex h-16 shrink-0 items-center">
           <nuxt-link href="/" class="cursor-default">
-            <p class="font-bold text-slate-800 tracking-wide">Cashflow a finance</p>
+            <p class="font-semibold text-slate-800 tracking-wide">Finanční řízení</p>
           </nuxt-link>
         </div>
         <nav class="flex flex-1 flex-col">
@@ -94,7 +94,7 @@
               <ul role="list" class="-mx-2 space-y-1">
                 <li v-for="item in navigation" :key="item.name">
                   <NuxtLink :href="item.href"
-                            class="text-gray-700 hover:bg-gray-100 hover:text-indigo-600 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-4"
+                            class="text-gray-500 hover:bg-gray-100 hover:text-indigo-600 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-4"
                             :active-class="'bg-gray-100 text-indigo-600'">
                     <component :is="item.icon"
                                class="group-hover:text-indigo-600 h-4 w-4 shrink-0"
