170c6d2a54a8c4ea4c211b918bd6451fc6b57f0d
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-22 15:16:39 +0200

    bank payment row

--- components/bank_payment/BankPaymentRow.vue
+++ components/bank_payment/BankPaymentRow.vue
@@ -1,23 +1,50 @@
 <template>
-  <div class="px-3 py-2 hover:bg-slate-50 flex justify-between items-center" @click="navigateToBankPayment">
-    <div class="text-sm flex">
-      <p class="text-gray-500 w-[60px] font-light">{{ bank_payment.id }}</p>
-      <p class="w-[400px]"></p>
-    </div>
+  <div class="px-3 py-2 hover:bg-slate-50 flex justify-between items-center">
+<!--    <div class="text-sm flex">-->
+<!--      <p class="text-gray-500 w-[60px] font-light">{{ bank_payment.id }}</p>-->
+<!--      <p class="w-[400px]"></p>-->
+<!--    </div>-->
 
-    <div class="text-sm text-slate-600 font-light flex items-center">
-      <p class="font-bold">{{formatPrice(bank_payment.amount)}} Kč</p>
-    </div>
+<!--    <div class="text-sm text-slate-600 font-light flex items-center">-->
+<!--      <p class="font-bold">{{ formatPrice(bank_payment.amount) }} Kč</p>-->
+<!--    </div>-->
+
+    <table class="w-full text-sm">
+      <tr @click="navigateToBankPayment">
+        <td class="w-[10%]">{{ formatDate(bank_payment.issued_at) }}</td>
+        <td class="w-[30%]">
+          <a class="text-dark text-decoration-none">{{ bank_payment.description }}</a>
+
+          <!--        @if($payment->received_invoices->count() > 0)-->
+          <!--        <br>-->
+
+          <!--        @foreach($payment->received_invoices as $received_invoice)-->
+          <!--        @include('admin.received_invoice.components.badge', ['received_invoice' => $received_invoice])-->
+          <!--        @endforeach-->
+          <!--        @endif-->
+        </td>
+        <td class="w-[20%]">
+          <span v-if="bank_payment.type === 'income'">{{ formatPrice(bank_payment.amount) }} Kč</span>
+          <span class="text-red-700" v-else>{{ formatPrice(bank_payment.amount) }} Kč</span>
+        </td>
+        <td class="w-[20%]">
+
+          <span class="badge text-bg-warning" v-if="bank_payment.paired_at === null">nespárováno</span>
+          <span class="badge text-bg-success" v-else>spárováno</span>
+        </td>
+
+      </tr>
+    </table>
   </div>
+
+
 </template>
 
 <script>
 import {defineComponent} from 'vue'
 import {DateTime} from 'luxon'
-import bank_accounts from "../../pages/bank_accounts/index.vue";
 
 export default defineComponent({
-  name: "BankPaymentRow",
   props: [
     'bank_payment'
   ],
