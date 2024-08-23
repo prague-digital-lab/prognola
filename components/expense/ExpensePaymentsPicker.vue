7ebe131b3746fa03eece80bf179a9f786ed4171b
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-23 17:18:44 +0200

    bank payment detail

--- components/expense/ExpensePaymentsPicker.vue
+++ components/expense/ExpensePaymentsPicker.vue
@@ -4,7 +4,7 @@ export default {
 
   methods: {
     formatPrice(value) {
-      let val = (value / 1).toFixed(0).replace('.', ',')
+      let val = (value / 1).toFixed(2).replace('.', ',')
       return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
     },
 
