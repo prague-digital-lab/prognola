0aeebebb95247322e4a707860220d201611856a0
Michael Dojčár <michaeldojcar@icloud.com>
2024-08-24 14:01:15 +0200

    fix bank payment sender message

--- pages/bank_payments/[bank_payment]/pair.vue
+++ pages/bank_payments/[bank_payment]/pair.vue
@@ -44,7 +44,7 @@
             <div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-900">Zpráva pro příjemce</dt>
               <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {{ bank_payment.sender_message }}
+                {{ bank_payment.sender_comment }}
               </dd>
             </div>
 
