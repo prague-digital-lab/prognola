f3b3927e75fbb540bc41b8e98ab783b7e3b2fc75
Michael Dojčár <michaeldojcar@icloud.com>
2024-09-24 19:07:57 +0200

    login page ui

--- pages/index.vue
+++ pages/index.vue
@@ -18,13 +18,18 @@ definePageMeta({
 
     <p class="text-[20px] text-gray-400 font-medium mb-[60px]">
       Už nemusíte přepínat mezi Slackem, Asanou a dalšími aplikacemi – a platit za každou zvlášť.
-      <br>Řiďte projekty, komunikujte se zákazníky, spravujte finance a spolupracujte se svým týmem efektivně na jednom místě.
+      <br>Řiďte projekty, komunikujte se zákazníky, spravujte finance a spolupracujte se svým týmem efektivně na jednom
+      místě.
     </p>
 
-    <a class="px-5 py-3 bg-gray-200 hover:bg-white text-gray-800 rounded-2xl transition duration-150 font-medium cursor-pointer">Vyzkoušet zdarma</a>
+    <nuxt-link href="/register"
+               class="px-5 py-3 bg-gray-200 hover:bg-white text-gray-800 rounded-2xl transition duration-150 font-medium cursor-pointer">
+      Vyzkoušet zdarma
+    </nuxt-link>
 
     <div class="mt-[100px] mb-10">
-    <img src="/img/dash_v1.png" class="max-w-full h-auto rounded-2xl shadow-2xl shadow-blue-700 opacity-70 hover:opacity-100 transition duration-500 border-gray-800">
+      <img src="/img/dash_v1.png"
+           class="max-w-full h-auto rounded-2xl shadow-2xl shadow-blue-700 opacity-70 hover:opacity-100 transition duration-500 border-gray-800">
     </div>
 
   </div>
---
+++ pages/register.vue
@@ -0,0 +1,95 @@
+<template>
+  <!--
+    This example requires updating your template:
+
+    ```
+    <html class="h-full bg-white">
+    <body class="h-full">
+    ```
+  -->
+  <div class="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+    <div class="w-full max-w-sm space-y-10">
+      <div>
+<!--        <img class="mx-auto h-10 w-auto" src="/img/logo_prazska_laborator.png" alt="Logo"/>-->
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-200">Registrace</h2>
+      </div>
+      <form class="space-y-6" v-on:submit.prevent="submitLogin">
+        <div class="relative -space-y-px rounded-md shadow-sm">
+          <div class="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300"/>
+          <div>
+            <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required=""
+                   class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                   placeholder="Emailová adresa"/>
+          </div>
+
+          <div>
+            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password"
+                   required=""
+                   class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                   placeholder="Heslo"/>
+          </div>
+        </div>
+
+        <!--        <div class="flex items-center justify-between">-->
+        <!--          <div class="flex items-center">-->
+        <!--            <input id="remember-me" name="remember-me" type="checkbox"-->
+        <!--                   class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>-->
+        <!--            <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-900">Remember me</label>-->
+        <!--          </div>-->
+
+        <!--          <div class="text-sm leading-6">-->
+        <!--            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>-->
+        <!--          </div>-->
+        <!--        </div>-->
+
+        <div>
+          <button type="submit"
+                  class="flex w-full justify-center rounded-md bg-gray-200 hover:bg-gray-100 px-3 py-2 text-sm leading-6 text-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Pokračovat
+          </button>
+        </div>
+      </form>
+
+      <!--      <p class="text-center text-sm leading-6 text-gray-500">-->
+      <!--        Not a member?-->
+      <!--        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14-day free trial</a>-->
+      <!--      </p>-->
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    definePageMeta({
+      layout: 'guest',
+    })
+
+    useHead({
+      title: 'Přihlášení - Prognola'
+    })
+  },
+
+  data: () => {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    async submitLogin() {
+      const {login} = useSanctumAuth();
+
+      const userCredentials = {
+        email: this.email,
+        password: this.password,
+      };
+
+      await login(userCredentials);
+    },
+  },
+
+}
+</script>
