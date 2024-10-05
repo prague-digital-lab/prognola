e40f1919aa6a7f15cd7a482b83aa482af81fffc6
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-05 17:54:29 +0200

    fix PRO-238: user can update fio api token

--- pages/[workspace]/bank_accounts/[bank_account]/edit.vue
+++ pages/[workspace]/bank_accounts/[bank_account]/edit.vue
@@ -1,8 +1,4 @@
 <template>
-  <Head>
-    <Title>{{ title }}</Title>
-  </Head>
-
   <div v-if="loaded">
     <page-content-header>
       <template v-slot:title>
@@ -21,6 +17,19 @@
         <div
           class="mb-7 rounded-md border border-gray-200 bg-white px-5 py-7 text-gray-700 md:w-1/2"
         >
+          <div class="mb-4">
+            <label
+              for="email"
+              class="block text-base font-medium leading-6 text-gray-900"
+              >Typ účtu</label
+            >
+            <div class="mt-2">
+              <p v-if="bank_account.bank === 'fio'" class="text-gray-500">
+                Fio Banka (automatická synchronizace)
+              </p>
+            </div>
+          </div>
+
           <div class="mb-4">
             <label
               for="email"
@@ -73,6 +82,29 @@
             </div>
           </div>
         </div>
+      </div>
+
+      <div class="mb-4" v-if="bank_account.bank === 'fio'">
+        <div
+          class="mb-7 rounded-md border border-gray-200 bg-white px-5 py-7 text-gray-700 md:w-1/2"
+        >
+          <div class="mb-4">
+            <label
+              for="email"
+              class="block text-base font-medium leading-6 text-gray-900"
+              >Token pro synchronizace s Fio Bankou</label
+            >
+
+            <div class="mt-2">
+              <input
+                required
+                v-model="api_token"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6"
+                placeholder=""
+              />
+            </div>
+          </div>
+        </div>
 
         <button
           class="cursor-pointer rounded-md bg-black px-4 py-2 font-medium text-white duration-200 hover:bg-gray-700"
@@ -92,6 +124,10 @@ definePageMeta({
   layout: "default",
   middleware: ["sanctum:auth", "sanctum:verified"],
 });
+
+useHead({
+  title: "Nastavení bankovního účtu",
+});
 </script>
 
 <script>
@@ -107,13 +143,14 @@ export default {
       name: "",
       account_number: "",
       bank_number: "",
+      api_token: "",
     };
   },
 
-  mounted() {
+  async mounted() {
     this.route = useRoute();
 
-    this.fetchData();
+    await this.fetchData();
     this.loaded = true;
   },
 
@@ -135,9 +172,11 @@ export default {
       );
 
       this.bank_account = data.value;
+
       this.name = data.value.name;
       this.account_number = data.value.account_number;
       this.bank_number = data.value.bank_number;
+      this.api_token = data.value.api_token;
     },
 
     async updateBankAccount() {
@@ -156,6 +195,7 @@ export default {
               name: this.name,
               account_number: this.account_number,
               bank_number: this.bank_number,
+              api_token: this.api_token,
             },
           },
         ),
