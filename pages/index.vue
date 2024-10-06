67cfaf1105c714194aa92f883d23fb3ac95b750f
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-06 17:32:12 +0200

    ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 export default {
   name: "index.vue",
 };
@@ -13,21 +12,22 @@ definePageMeta({
 
 <template>
   <div>
-    <h1 class="mb-[40px] text-[45px] font-medium text-white">
-      Plánujte firemní finance jako nikdy předtím
+    <h1 class="mb-[40px] text-[40px] font-medium text-white">
+      Budoucnost řízení firemních financí je tady.
     </h1>
 
-    <p class="mb-[60px] text-[20px] font-medium text-gray-400">
-      Získejte zpět přehled o aktuálním stavu financí ve vaší firmě.
-      <br />, kolik budete mít peněz v nadcházejících
-      obdobích.
+    <p class="mb-[60px] text-[18px] text-gray-300">
+      Představte si, že už nejste nervózní z toho, jak vaše firma v
+      budoucnu vyjde s penězi. <br />
+      Prognola je digitální finanční centrum pro firmy, kterým už nestačí znát
+      zůstatek na firemním účtu.
     </p>
 
     <nuxt-link
       href="/register"
       class="cursor-pointer rounded-2xl bg-gray-200 px-5 py-3 font-medium text-gray-800 transition duration-150 hover:bg-white"
     >
-      Vyzkoušet zdarma
+      Vyzkoušet na 30 dní zdarma
     </nuxt-link>
 
     <div class="mb-10 mt-[100px]">
