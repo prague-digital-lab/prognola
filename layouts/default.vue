b848afce600d537737451ecb91eb7559597dedcf
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-09 20:21:57 +0200

    ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -127,12 +127,12 @@
     >
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div
-        class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-zinc-100 px-6 pb-4"
+        class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-zinc-100 px-4 pb-4"
       >
         <div class="flex h-16 shrink-0 items-center">
           <nuxt-link
             :href="'/' + route.params.workspace + '/cashflow'"
-            class="flex cursor-default items-center"
+            class="flex cursor-default items-center rounded-md py-1 pe-3 ps-2 duration-100 hover:bg-gray-200/50 active:hover:bg-gray-200/80"
           >
             <nuxt-img
               src="/img/logo_prazska_laborator.png"
@@ -145,7 +145,7 @@
             </p>
           </nuxt-link>
         </div>
-        <nav class="flex flex-1 flex-col">
+        <nav class="flex flex-1 flex-col px-2">
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
             <li>
               <ul role="list" class="-mx-2 space-y-1">
