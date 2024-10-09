1b9d8a781e459f5062677dcddc0aaece2b102910
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-09 17:06:16 +0200

    fix account number input

--- pages/[workspace]/bank_accounts/new.vue
+++ pages/[workspace]/bank_accounts/new.vue
@@ -95,7 +95,7 @@
                 <input
                   v-model="account_number"
                   required
-                  type="number"
+                  type="text"
                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6"
                   placeholder="1234567890"
                 />
