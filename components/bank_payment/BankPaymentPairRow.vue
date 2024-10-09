888249ac0ee6ee3ae2e04a4139503a5d8429dd8c
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-09 19:13:03 +0200

    show more details in bank pairing

--- components/bank_payment/BankPaymentPairRow.vue
+++ components/bank_payment/BankPaymentPairRow.vue
@@ -60,7 +60,7 @@ export default defineComponent({
   props: ["bank_payment"],
   methods: {
     formatPrice(value) {
-      let val = (value / 1).toFixed(0).replace(".", ",");
+      let val = (value / 1).toFixed(2).replace(".", ",");
       return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     },
 
