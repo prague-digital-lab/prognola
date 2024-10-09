9e9e814f31afe75d31fb33ffab044ad74c267a92
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-09 17:27:44 +0200

    fix PRO-305: organisation label

--- pages/[workspace]/organisations/[organisation].vue
+++ pages/[workspace]/organisations/[organisation].vue
@@ -23,7 +23,10 @@
         </div>
 
 <!--        <p class="mb-5 text-base text-gray-500">{{ organisation.type }}</p>-->
-        <p class="mb-5 text-base text-gray-500">Organizace</p>
+        <p class="mb-5 text-base text-gray-500" v-if="organisation.expenses.length === 0 && organisation.incomes.length === 0">Organizace</p>
+        <p class="mb-5 text-base text-gray-500" v-if="organisation.expenses.length > 0 && organisation.incomes.length === 0">Dodavatel</p>
+        <p class="mb-5 text-base text-gray-500" v-if="organisation.expenses.length === 0 && organisation.incomes.length > 0">Zákazník</p>
+        <p class="mb-5 text-base text-gray-500" v-if="organisation.expenses.length > 0 && organisation.incomes.length > 0">Dodavatel a zákazník</p>
 
         <textarea
           v-model="input_internal_note"
