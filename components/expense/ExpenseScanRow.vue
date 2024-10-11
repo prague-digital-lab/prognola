7b629c466ed983f5049ccdb5617d7df33fd5e0b8
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-11 15:17:02 +0200

    ref PRO-323: file upload ui

--- components/expense/ExpenseScanRow.vue
+++ components/expense/ExpenseScanRow.vue
@@ -1,23 +1,20 @@
-<script>
-export default {
-  props: ["scan"],
-
-  data: () => {
-    return {
-      expanded: false,
-    };
-  },
-};
-</script>
-
 <template>
   <div class="w-full text-base text-slate-500">
     <div
       class="flex items-center justify-between border-b border-gray-200 bg-white px-5 py-2 hover:bg-gray-hover active:bg-gray-100"
       @click="!expanded ? (expanded = true) : (expanded = false)"
     >
-      <div>
-        {{ scan.title }}</div>
+      <div class="flex items-center">
+        {{ scan.title }}
+        <a
+          :href="scan.file_url"
+          title="Otevřít v novém panelu"
+          target="_blank"
+          class="ms-2 inline-block rounded-md p-1 hover:bg-gray-100 active:bg-gray-200"
+        >
+          <LinkIcon class="h-4 w-4" />
+        </a>
+      </div>
 
       <div>
         <div v-if="expanded">
@@ -66,5 +63,21 @@ export default {
     </div>
   </div>
 </template>
+
+<script setup>
+import { LinkIcon } from "@heroicons/vue/16/solid";
+</script>
+
+<script>
+export default {
+  props: ["scan"],
+
+  data: () => {
+    return {
+      expanded: false,
+    };
+  },
+};
+</script>
 
 <style scoped></style>
