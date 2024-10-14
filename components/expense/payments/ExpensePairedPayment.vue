dd247af00e5a447a2b239d5cc9319eeb5c3157e0
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-14 14:10:42 +0200

    fix PRO-329: show date at paired payments

--- components/expense/payments/ExpensePairedPayment.vue
+++ components/expense/payments/ExpensePairedPayment.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-56">
-    <Menu as="div" class="relative inline-block">
-      <div>
-        <MenuButton class="">
-          <p
-            class="mb-1 rounded px-1 py-1 text-sm text-gray-800 hover:bg-gray-100"
-          >
+  <Menu as="div" class="relative">
+    <div>
+      <MenuButton class="w-full">
+        <div
+          class="mb-1 flex justify-between rounded px-1 py-1 text-sm text-gray-800 hover:bg-gray-100 pe-2"
+        >
+          <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -20,53 +20,58 @@
             </svg>
 
             {{ formatPrice(payment.amount) }} Kč
-          </p>
-        </MenuButton>
-      </div>
-
-      <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
-      >
-        <MenuItems
-          class="absolute right-0 top-6 z-40 mt-2 w-56 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
-        >
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-                @click="navigateToPayment(payment)"
-              >
-                Přejít na stránku platby
-              </button>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
-                @click="unpairPayment(payment)"
-              >
-                Zrušit párování platby
-              </button>
-            </MenuItem>
           </div>
-        </MenuItems>
-      </transition>
-    </Menu>
-  </div>
+
+          <div class="text-gray-500 text-sm">
+            {{ formatDate(payment.issued_at) }}
+          </div>
+        </div>
+      </MenuButton>
+    </div>
+
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform scale-100 opacity-100"
+      leave-to-class="transform scale-95 opacity-0"
+    >
+      <MenuItems
+        class="absolute right-0 top-6 z-40 mt-2 w-56 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+      >
+        <div class="px-1 py-1">
+          <MenuItem v-slot="{ active }">
+            <button
+              :class="[
+                active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+              ]"
+              @click="navigateToPayment(payment)"
+            >
+              Přejít na stránku platby
+            </button>
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+            <button
+              :class="[
+                active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+              ]"
+              @click="unpairPayment(payment)"
+            >
+              Zrušit párování platby
+            </button>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
 </template>
 
 <script setup>
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import { DateTime } from "luxon";
 
 defineProps(["payment"]);
 const emit = defineEmits(["expense-updated"]);
@@ -98,11 +103,17 @@ async function unpairPayment(payment) {
     ),
   );
 
-  emit('expense-updated');
+  emit("expense-updated");
 }
 
 function formatPrice(value) {
   let val = (value / 1).toFixed(2).replace(".", ",");
   return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function formatDate(date) {
+  let formatted = DateTime.fromISO(date);
+
+  return formatted.toFormat("d.M.yyyy");
 }
 </script>
