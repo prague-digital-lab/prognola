1efaf6635ccbe80cd643f662fcfc8658aa6c9526
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-21 17:24:14 +0200

    ui

--- pages/[workspace]/cashflow.vue
+++ pages/[workspace]/cashflow.vue
@@ -6,7 +6,7 @@
           <h2
             class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:tracking-tight"
           >
-            Cashflow
+            Cash flow
           </h2>
         </template>
 
