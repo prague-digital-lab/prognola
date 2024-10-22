d091b759ffae7b91f987690ed5b3a64da766865d
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-22 13:00:22 +0200

    fix PRO-357: add precision to bank payment detail

--- pages/[workspace]/bank_payments/[bank_payment]/index.vue
+++ pages/[workspace]/bank_payments/[bank_payment]/index.vue
@@ -239,7 +239,7 @@ export default {
     },
 
     formatPrice(value) {
-      let val = (value / 1).toFixed(0).replace(".", ",");
+      let val = (value / 1).toFixed(2).replace(".", ",");
       return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     },
   },
