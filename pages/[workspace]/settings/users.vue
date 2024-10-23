60e8a0acfa69e6c76fd0f105060e6f6be2e06f28
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-23 18:41:02 +0200

    ref PRO-348: invite email validation

--- pages/[workspace]/settings/users.vue
+++ pages/[workspace]/settings/users.vue
@@ -50,6 +50,7 @@
           <div class="flex justify-between">
             <input
               required
+              type="email"
               placeholder="Email uživatele"
               v-model="invited_user_email"
               class="me-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6"
