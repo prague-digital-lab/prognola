4c05f5593458f18ce36910ce73d580423fe2dae9
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-24 12:44:40 +0200

    ui

--- components/badges/BadgeBankAccount.vue
+++ components/badges/BadgeBankAccount.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link
     @click="navigateToBankAccount"
-    class="cursor-pointer select-none rounded-md border border-gray-200 bg-white px-2 py-1 text-gray-700 duration-100 hover:bg-gray-hover active:bg-gray-100 dark:border-zinc-800 dark:bg-zinc-900 dark:hover:bg-zinc-800 dark:text-zinc-300"
+    class="cursor-pointer select-none rounded-md border border-gray-200 bg-white px-2 py-1 text-gray-700 duration-100 hover:bg-gray-hover active:bg-gray-100 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:text-zinc-300"
   >
     {{ bank_account.name }}
   </nuxt-link>
