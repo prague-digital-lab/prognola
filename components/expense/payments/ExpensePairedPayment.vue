a72e42ece74b09fa5e47dc9b438d86d7bbf69f25
Michael Dojčár <michaeldojcar@icloud.com>
2024-10-24 11:55:31 +0200

    ui

--- components/expense/payments/ExpensePairedPayment.vue
+++ components/expense/payments/ExpensePairedPayment.vue
@@ -5,19 +5,9 @@
         <div
           class="mb-1 flex justify-between rounded px-1 py-1 pe-2 text-sm text-gray-800 hover:bg-gray-100 dark:text-zinc-400 dark:hover:bg-zinc-900"
         >
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="me-2 inline-block size-5 text-gray-600"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M2.25 2.25a.75.75 0 0 0 0 1.5H3v10.5a3 3 0 0 0 3 3h1.21l-1.172 3.513a.75.75 0 0 0 1.424.474l.329-.987h8.418l.33.987a.75.75 0 0 0 1.422-.474l-1.17-3.513H18a3 3 0 0 0 3-3V3.75h.75a.75.75 0 0 0 0-1.5H2.25Zm6.54 15h6.42l.5 1.5H8.29l.5-1.5Zm8.085-8.995a.75.75 0 1 0-.75-1.299 12.81 12.81 0 0 0-3.558 3.05L11.03 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l2.47-2.47 1.617 1.618a.75.75 0 0 0 1.146-.102 11.312 11.312 0 0 1 3.612-3.321Z"
-                clip-rule="evenodd"
-              />
-            </svg>
+          <div class="flex items-center">
+            <minus-circle-icon v-if="payment.type === 'expense'" class="h-4 w-4 me-1 inline-block dark:text-red-500"></minus-circle-icon>
+            <plus-circle-icon v-if="payment.type === 'income'" class="h-4 w-4 me-1 inline-block dark:text-blue-500"></plus-circle-icon>
 
             {{ formatPrice(payment.amount) }} Kč
           </div>
@@ -71,6 +61,7 @@
 
 <script setup>
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import {MinusCircleIcon, PlusCircleIcon} from "@heroicons/vue/20/solid";
 import { DateTime } from "luxon";
 
 defineProps(["payment"]);
