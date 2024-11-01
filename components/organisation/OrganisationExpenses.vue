f068e3db7a04f128e3954bf9408c026f45b99d40
Michael Dojčár <michaeldojcar@icloud.com>
2024-11-01 20:58:03 +0100

    fix PRO-409: expense local date pickers

--- components/organisation/OrganisationExpenses.vue
+++ components/organisation/OrganisationExpenses.vue
@@ -5,7 +5,7 @@
         <p class="text-base text-gray-600 dark:text-zinc-400">Výdaje</p>
       </div>
       <div>
-        <select v-if="date_type" @change="changeDateType" class="border-gray-200 border text-sm py-0 me-2 rounded-md h-[30px]">
+        <select v-model="date_type" v-if="date_type" @change="changeDateType" class="border-gray-200 border text-sm py-0 me-2 rounded-md h-[30px]">
           <option value="paid_at">Datum platby</option>
           <option value="received_at">Datum přijetí</option>
         </select>
@@ -46,6 +46,10 @@ function openModal() {
 }
 
 onMounted(() => {
+  if(localStorage.getItem("date_type") !== undefined) {
+    date_type.value = localStorage.getItem("date_type")
+  }
+
   fetchData();
 });
 
@@ -54,6 +58,8 @@ async function fetchData() {
 }
 
 function changeDateType(event) {
+  localStorage.setItem('date_type', event.target.value);
+
   date_type.value = event.target.value;
   fetchData();
 }
