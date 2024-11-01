ea560bf90b75a703433d791034b378501cf9c4f5
Michael Dojčár <michaeldojcar@icloud.com>
2024-11-01 21:23:18 +0100

    fix PRO-411: nuxt Sentry integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,14 @@ export default defineNuxtConfig({
   css: ["~/assets/scss/app.scss"],
   ssr: false,
 
+  sentry: {
+    sourceMapsUploadOptions: {
+      org: "prazska-laborator",
+      project: "prognola-web",
+      authToken: "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",
+    },
+  },
+
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -34,6 +42,6 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ["nuxt-auth-sanctum", "@nuxt/image", "@nuxt/fonts"],
+  modules: ["nuxt-auth-sanctum", "@nuxt/image", "@nuxt/fonts", "@sentry/nuxt/module"],
   compatibilityDate: "2024-07-16",
 });
---
+++ sentry.client.config.ts
@@ -0,0 +1,16 @@
+import * as Sentry from "@sentry/nuxt";
+
+Sentry.init({
+  // If set up, you can use your runtime config here
+  // dsn: useRuntimeConfig().public.sentry.dsn,
+  dsn: "https://e576359b7ce1dc9c9994707518553800@o4505390391754752.ingest.us.sentry.io/4508224870285312",
+  integrations: [Sentry.replayIntegration()],
+  // Tracing
+  // We recommend adjusting this value in production, or using a tracesSampler for finer control.
+  tracesSampleRate: 0.1, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
