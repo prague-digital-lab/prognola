2326109f70f7944ae26798b6c3b03777a5e4c94a
Michael Dojčár <michaeldojcar@icloud.com>
2024-11-05 11:20:29 +0100

    fix PRO-445: public footer

--- components/guest/Footer.vue
+++ components/guest/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border-t border-zinc-700 bg-black p-10">
     <div class="container mx-auto text-white">
-      <div class="grid grid-cols-4 gap-x-5">
+      <div class="grid grid-cols-4 gap-x-5 mx-10">
         <div class="text-base text-zinc-400">
           <h2 class="mb-4 text-xl text-white">Prognola</h2>
 
@@ -15,7 +15,7 @@
 
           <div class="flex flex-col space-y-2">
             <nuxt-link href="/cs/plan">Plánování</nuxt-link>
-            <nuxt-link href="/cs/prijmy-a-vydaje">Příjmy a výdaje</nuxt-link>
+            <nuxt-link href="/cs/incomes-and-expenses">Příjmy a výdaje</nuxt-link>
             <nuxt-link href="/cs/changelog">Novinky v aplikaci</nuxt-link>
           </div>
         </div>
