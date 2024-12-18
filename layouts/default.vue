d862a383c1a81de538be98980da031f9e0b72bbe
Michael Dojčár <michaeldojcar@icloud.com>
2024-12-18 14:36:27 +0100

    fix PRO-126: responsive menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,71 +49,23 @@
               </TransitionChild>
               <!-- Sidebar component, swap this element with another sidebar if you like -->
               <div
-                class="flex grow flex-col overflow-y-auto bg-white px-6 pb-4"
+                class="flex grow flex-col overflow-y-auto bg-white py-4"
               >
-                <div class="flex shrink-0 items-center">
-                  <nuxt-link href="/">
-                    <img
-                      class="h-8 w-auto"
-                      src="/img/logo_horizontal_sm.webp"
-                      alt="Prognola"
-                    />
-                  </nuxt-link>
-                </div>
-                <nav class="flex flex-1 flex-col">
-                  <ul role="list" class="flex flex-1 flex-col gap-y-7">
-                    <li>
-                      <ul role="list" class="-mx-2 space-y-1">
-                        <li v-for="item in navigation" :key="item.name">
-                          <NuxtLink
-                            :href="item.href"
-                            :class="[
-                              item.current
-                                ? 'bg-zinc-50 font-medium text-indigo-600'
-                                : 'text-zinc-700 hover:bg-zinc-50 hover:text-indigo-600',
-                              'group flex gap-x-3 rounded-md p-2 text-base font-semibold leading-6',
-                            ]"
-                          >
-                            <component
-                              :is="item.icon"
-                              :class="[
-                                item.current
-                                  ? 'text-indigo-600'
-                                  : 'text-zinc-400 group-hover:text-indigo-600',
-                                'h-6 w-6 shrink-0',
-                              ]"
-                              aria-hidden="true"
-                            />
-                            {{ item.name }}
-                          </NuxtLink>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <!--                      <div class="text-xs font-semibold leading-6 text-zinc-400">Your teams</div>-->
-                      <!--                      <ul role="list" class="-mx-2 mt-2 space-y-1">-->
-                      <!--                        <li v-for="team in teams" :key="team.name">-->
-                      <!--                          <NuxtLink :href="team.href"-->
-                      <!--                                    :class="[team.current ? 'bg-zinc-50 text-indigo-600' : 'text-zinc-700 hover:bg-zinc-50 hover:text-indigo-600', 'group flex gap-x-3 rounded-md p-2 text-base font-semibold leading-6']">-->
-                      <!--                            <span-->
-                      <!--                                :class="[team.current ? 'border-indigo-600 text-indigo-600' : 'border-zinc-200 text-zinc-400 group-hover:border-indigo-600 group-hover:text-indigo-600', 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium']">{{-->
-                      <!--                                team.initial-->
-                      <!--                              }}</span>-->
-                      <!--                            <span class="truncate">{{ team.name }}</span>-->
-                      <!--                          </NuxtLink>-->
-                      <!--                        </li>-->
-                      <!--                      </ul>-->
-                    </li>
-                    <li class="mt-auto">
-                      <!--                      <NuxtLink href="#"-->
-                      <!--                                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-base font-semibold leading-6 text-zinc-700 hover:bg-zinc-50 hover:text-indigo-600">-->
-                      <!--                        <Cog6ToothIcon class="h-6 w-6 shrink-0 text-zinc-400 group-hover:text-indigo-600"-->
-                      <!--                                       aria-hidden="true"/>-->
-                      <!--                        Settings-->
-                      <!--                      </NuxtLink>-->
-                    </li>
-                  </ul>
-                </nav>
+                <nuxt-link
+                  :href="'/' + route.params.workspace + '/cashflow'"
+                  class="flex w-full cursor-default items-center rounded-md py-1 pe-3 ps-4 mb-5 duration-100 hover:bg-zinc-200/50 active:hover:bg-zinc-200/80 dark:hover:bg-zinc-800 dark:active:hover:bg-zinc-700/80"
+                >
+                  <nuxt-img
+                    src="/img/logo_prazska_laborator.png"
+                    width="20"
+                    class="me-4 ms-1"
+                  />
+
+                  <p class="text-[15px] text-zinc-800 dark:text-zinc-200">Prognola</p>
+                </nuxt-link>
+
+
+                <sidebar></sidebar>
               </div>
             </DialogPanel>
           </TransitionChild>
